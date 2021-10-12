8aead82695e0ac21ec05af51ad2302a771f482db
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2021-10-12 08:49:04 +0000

    First Upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const inputField = document.getElementById('inputField');
+const addTodo = document.getElementById('addTodo');
+const todoBucket = document.getElementById('todoBucket');
+
+addTodo.addEventListener('click',  ()=> {
+
+})
